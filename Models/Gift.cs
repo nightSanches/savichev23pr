5d11a879e068ecef8cac85ca57191b0f370adcc0
gsavichev <galesavich@gmail.com>
2024-01-24 00:30:15 +0500

    Реализована модель Gifts

---
+++ Models/Gift.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace savichev23pr.Models
+{
+    public class Gift
+    {
+        public int id { get; set; }
+        public string fio { get; set; }
+        public string message { get; set; }
+        public string adress { get; set; }
+        public DateTime datetime { get; set; }
+        public string email { get; set; }
+    }
+}
